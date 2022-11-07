fb3cc20d5af826ac91ceb64db6d5c8c260ed6ec8
Régis Terrier <rterrier@apc.in2p3.fr>
2022-11-07 08:53:32 +0100

    Recompiled docs for 1.0rc2

--- docs/1.0rc2/_static/documentation_options.js
+++ docs/1.0rc2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'X.Y.Z',
+    VERSION: '1.0rc2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
