e84ab1024e4a413087c9d7ac6a05953295f624fd
Quentin Remy <quentin.remy@mpi-hd.mpg.de>
2024-11-28 13:19:27 +0100

    Remove old doc versions
    Remove old docs versions and keep only those displayed in the switcher

--- docs/0.6/api/gammapy-time-plot_fermi_3fgl_light_curve-1.py
+++
@@ -1,7 +0,0 @@
-from gammapy.time import plot_fermi_3fgl_light_curve
-plot_fermi_3fgl_light_curve('3FGL J0349.9-2102',
-                            time_start='2010-01-01',
-                            time_end='2015-02-02')
-
-import matplotlib.pyplot as plt
-plt.show()
