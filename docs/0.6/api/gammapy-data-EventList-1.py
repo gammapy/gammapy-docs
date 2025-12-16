cea60abb56365d7d96069d1d941384aa51c3f17f
Régis Terrier <rterrier@apc.in2p3.fr>
2025-12-16 13:39:37 +0100

    Remove 0.6
    Signed-off-by: Régis Terrier <rterrier@apc.in2p3.fr>

--- docs/0.6/api/gammapy-data-EventList-1.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from gammapy.data import DataStore
-
-ds = DataStore.from_dir('$GAMMAPY_EXTRA/datasets/hess-crab4-hd-hap-prod2')
-events = ds.obs(obs_id=23523).events
-events.plot_time_map()
-plt.show()
